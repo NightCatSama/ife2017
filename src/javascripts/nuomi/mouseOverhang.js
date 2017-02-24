3e05722e9b12cdcc09c0d096a79098e07252898f
陈慕杰 <642163903@qq.com>
2017-02-24 19:20:22 +0800

    modify mouseoverhang

--- src/javascripts/nuomi/mouseOverhang.js
+++ src/javascripts/nuomi/mouseOverhang.js
@@ -24,13 +24,13 @@ function setStyles(pos, center) {
 	}
 	let x = ~~((pos.px - center.cx) / (width / 2) * 8)
 	let y = ~~((pos.py - center.cy) / (height / 2) * 8)
-	let deg = getAngle(pos, center)
+	let deg = -getAngle(pos, center)
 	let intensity = getDistance(pos, center) / diagonal / 2
 	title.style.cssText = `
 		background-image: linear-gradient(${deg}deg, rgba(255, 255, 255, ${intensity}) 0%, rgba(255, 255, 255, 0) 80%);
 		background-image: -webkit-linear-gradient(${deg}deg, rgba(255, 255, 255, ${intensity / 2}) 0%, rgba(255, 255, 255, 0) 80%);
-		transform: rotateX(${x}deg) rotateY(${y}deg) rotateZ(0);
-		-webkit-transform: rotateX(-${x}deg) rotateY(-${y}deg) rotateZ(0);
+		transform: translateY(${pos.py / height * -0.5}px) rotateX(${pos.py < center.cy ? -x : x}deg) rotateY(${y}deg) rotateZ(0);
+		-webkit-transform: translateY(${pos.py / height * -0.5}px) rotateX(${pos.py < center.cy ? -x : x}deg) rotateY(${y}deg) rotateZ(0);
 	`
 }
 
@@ -46,10 +46,21 @@ container.addEventListener('mouseleave', (e) => {
 	setStyles()
 })
 
-function getAngle({ px, py }, { cx, cy }) {
-    return ~~((Math.atan2(py - cy, px - cx)) / Math.PI * 180)
-}
-
 function getDistance({ px, py }, { cx, cy }) {
     return ~~Math.sqrt(Math.pow((px - cx), 2) + Math.pow((py - cy), 2))
+}
+
+function getAngle({ px, py }, { cx, cy }) {
+	let x = px - cx
+	let y = py - cy
+	let hypotenuse = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+	let cos = x / hypotenuse
+	let radian = Math.acos(cos)
+	let angle = 180 / (Math.PI / radian)
+	if (y < 0) {
+		angle = -angle
+	} else if ((y == 0) && (x < 0)) {
+		angle = 180
+	}
+	return angle
 }
